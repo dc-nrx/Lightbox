b516eaa914790472bff181a91eb0478bfb888a07
dima chapovsky <dc_dc@protonmail.com>
2020-12-09 17:10:25 +0200

    to previous

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
             targets: ["Lightbox"]),
     ],
     dependencies: [
-      .package(url: "https://github.com/dc-nrx/Imaginary", .branch("master"))
+      .package(url: "https://github.com/dc-nrx/Imaginary", .revision("86b35053e6038c4e18c125ffba5abc97f8b249c8"))
     ],
     targets: [
         .target(
